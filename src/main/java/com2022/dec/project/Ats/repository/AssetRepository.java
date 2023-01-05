45b6c6c2e1531da002aea17fbd5a63fdf8696492
m-afzar <121795607+m-afzar@users.noreply.github.com>
2023-01-05 16:06:48 +0530

    New one

--- src/main/java/com2022/dec/project/Ats/repository/AssetRepository.java
+++ src/main/java/com2022/dec/project/Ats/repository/AssetRepository.java
@@ -3,6 +3,7 @@ package com2022.dec.project.Ats.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com2022.dec.project.Ats.model.Asset;
+// test
 
 public interface AssetRepository extends MongoRepository<Asset, String>{
 	
